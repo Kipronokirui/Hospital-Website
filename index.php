f225eede4f6d06ed4e6948e78b6a2746c1efbef4
Eduin <victorkipronokirui2@gmail.com>
2022-09-27 20:11:14 +0300

    Fifth Commit

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 <?php include_once('./Hospital/index.html');?>
+<?php include_once('./Hospital/about.html');?>
+<?php include_once('./Hospital/careers.html');?>
+<?php include_once('./Hospital/qualities.html');?>
+<?php include_once('./Hospital/services.html');?> 
